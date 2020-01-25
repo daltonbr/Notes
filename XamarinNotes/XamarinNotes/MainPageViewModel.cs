9bde1965e8cbb58baaf5c85b130484a6bd6474c8
Dalton Lima <daltonvarussa@gmail.com>
2020-01-25 20:49:18 +0100

    Add XamarinNotes project (initial UI)

---
+++ XamarinNotes/XamarinNotes/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace XamarinNotes
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+            });
+        }
+
+        public ObservableColletion<string> AllNotes { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string theNote;
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        Command SaveCommand { get; }
+        Command EraseCommand { get; }
+    }
+}
